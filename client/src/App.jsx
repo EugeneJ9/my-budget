5a44787fee2e791346b72f2720072976bfc841d3
EugeneJ9 <111596903+EugeneJ9@users.noreply.github.com>
2022-11-11 20:43:45 +0200

    Add new route (#1)
    * Add new route
    
    * Fix comment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,13 @@ import { Routes, Route, Link } from 'react-router-dom';
 import AddTutorial from './components/AddTutorial';
 import Tutorial from './components/Tutorial';
 import TutorialsList from './components/TutorialsList';
+import Categories from './components/Categories';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
-    console.log('render,app');
     return (
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
@@ -28,6 +28,11 @@ class App extends Component {
                 Add
               </Link>
             </li>
+            <li className="nav-item">
+              <Link to="/categories" className="nav-link">
+                Categories
+              </Link>
+            </li>
           </div>
         </nav>
 
@@ -36,6 +41,7 @@ class App extends Component {
             <Route path="/" element={<TutorialsList />} />
             <Route path="/tutorials" element={<TutorialsList />} />
             <Route path="/add" element={<AddTutorial />} />
+            <Route path="/categories" element={<Categories />} />
             <Route path="/tutorials/:id" element={<Tutorial />} />
           </Routes>
         </div>
